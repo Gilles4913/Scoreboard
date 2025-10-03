a3ffc59e2348a41b9d9359eaa9d0d91992bc9b97
Gilles4913 <gilles.guerrin@a2display.fr>
2025-10-03 11:24:45 +0200

    Rediriger directement vers l'authentification Operator

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: { 
     port: 3000,
-    host: true,
-    open: 'http://localhost:5173'
+    host: true
   },
   build: {
     outDir: 'dist'
