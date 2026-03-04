f3aa13ed5871a47a76215375f3256a84b0fa5501
Gilles4913 <gilles.guerrin@a2display.fr>
2026-03-04 14:25:32 +0100

    Add environment variable exports to config.ts

---
+++ apps/home/src/config.ts
@@ -0,0 +1,3 @@
+export const OPERATOR_URL = (import.meta.env.VITE_OPERATOR_URL as string) || "";
+export const DISPLAY_URL = (import.meta.env.VITE_DISPLAY_URL as string) || "";
+export const ADMIN_URL = (import.meta.env.VITE_ADMIN_URL as string) || "";
