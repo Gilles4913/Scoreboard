1cad8dde872e4731b8f6fc1d812de37463747524
Gilles4913 <gilles.guerrin@a2display.fr>
2026-03-04 18:41:30 +0100

    Update App.tsx

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,12 +1,41 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminHome from "./AdminHome";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SuperAdminGuard from "./guards/SuperAdminGuard";
+import AdminLayout from "./components/AdminLayout";
+import OrgsPage from "./pages/OrgsPage";
+import MembersPage from "./pages/MembersPage";
+import SportsPage from "./pages/SportsPage";
+
+function Home() {
+  return (
+    <div>
+      <h1>Scoreboard Admin Console</h1>
+      <p>Interface Super Admin pour gérer :</p>
+      <ul>
+        <li>Organisations</li>
+        <li>Membres</li>
+        <li>Sports</li>
+        <li>Paramètres d'affichage (phase suivante)</li>
+      </ul>
+      <p style={{ marginTop: 12, fontWeight: 700 }}>Utilise le menu en haut.</p>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AdminHome />} />
-      </Routes>
+      <SuperAdminGuard>
+        <AdminLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/orgs" element={<OrgsPage />} />
+            <Route path="/members" element={<MembersPage />} />
+            <Route path="/sports" element={<SportsPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </AdminLayout>
+      </SuperAdminGuard>
     </BrowserRouter>
   );
 }
