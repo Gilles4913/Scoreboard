e85d125406e0de95698ca8cd64ce03b29cbb37ee
Gilles4913 <gilles.guerrin@a2display.fr>
2026-03-05 19:06:32 +0100

    Update App.tsx

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,46 +1,26 @@
+// apps/admin/src/App.tsx
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import SuperAdminGuard from "./guards/SuperAdminGuard";
 import AdminLayout from "./components/AdminLayout";
+
+import DashboardPage from "./pages/DashboardPage";
 import OrgsPage from "./pages/OrgsPage";
 import MembersPage from "./pages/MembersPage";
 import SportsPage from "./pages/SportsPage";
-import DashboardPage from "./pages/DashboardPage";
-
-function Home() {
-  return (
-    <div>
-      <h1>Scoreboard Admin Console</h1>
-      <p>Interface Super Admin pour gérer :</p>
-      <ul>
-        <li>Organisations</li>
-        <li>Membres</li>
-        <li>Sports</li>
-        <li>Paramètres d'affichage (phase suivante)</li>
-      </ul>
-      <p style={{ marginTop: 12, fontWeight: 700 }}>Utilise le menu en haut.</p>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <BrowserRouter>
       <SuperAdminGuard>
         <AdminLayout>
-          <Routes
-            element={
-              <SuperAdminGuard>
-                <AdminLayout />
-              </SuperAdminGuard>
-            }>
-            <Route path="/" element={<Home />} />
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
             <Route path="/orgs" element={<OrgsPage />} />
             <Route path="/members" element={<MembersPage />} />
             <Route path="/sports" element={<SportsPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
           </Routes>
         </AdminLayout>
       </SuperAdminGuard>
